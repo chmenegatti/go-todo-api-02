6f2424773aed5bffdcbe8c7181b95645ed2e73e3
Cesar Menegatti <cesar.oliveira@totvs.com.br>
2024-09-20 08:53:51 -0300

    ✨ (todo_controller.go): Adiciona funcionalidade de CRUD para todos os todos 🔧 (main.go): Adiciona conexão com o banco de dados antes de inicializar o servidor Gin

--- controllers/todo_controller.go
+++ controllers/todo_controller.go
@@ -1,31 +1,65 @@
 package controllers
 
 import (
+	"go-todo-api-02/database"
+	"go-todo-api-02/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetAllTodos(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Retorna todos os todos"})
+	var todos []models.Todo
+	database.DB.Find(&todos)
+	c.JSON(http.StatusOK, todos)
 }
 
 // CreateTodo cria um novo todo
 func CreateTodo(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{"message": "Todo criado"})
+	var todo models.Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	database.DB.Create(&todo)
+	c.JSON(http.StatusCreated, todo)
 }
 
 // GetTodoByID retorna um todo pelo ID
 func GetTodoByID(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Retorna um todo pelo ID"})
+	var todo models.Todo
+	id := c.Param("id")
+	if err := database.DB.First(&todo, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo não encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, todo)
 }
 
 // UpdateTodo atualiza um todo
 func UpdateTodo(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Todo atualizado"})
+	var todo models.Todo
+	id := c.Param("id")
+	if err := database.DB.First(&todo, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo não encontrado"})
+		return
+	}
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	database.DB.Save(&todo)
+	c.JSON(http.StatusOK, todo)
 }
 
 // DeleteTodo deleta um todo
 func DeleteTodo(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Todo deletado"})
+	var todo models.Todo
+	id := c.Param("id")
+	if err := database.DB.First(&todo, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo não encontrado"})
+		return
+	}
+	database.DB.Delete(&todo)
+	c.JSON(http.StatusOK, gin.H{"message": "Todo deletado com sucesso"})
 }
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"go-todo-api-02/database"
 	"go-todo-api-02/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	// Conecta ao banco de dados
+	database.Connect()
+
 	// Inicializa o router Gin
 	router := gin.Default()
 
